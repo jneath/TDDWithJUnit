127c698c109849d6fe2daa7ed9de9aacfe7834e3
jneath <john.neath@yahoo.com>
2015-04-08 15:02:49 -0500

    Implemented cannotDeceleratBeyondMinSpeed

--- test/CarTest.java
+++ test/CarTest.java
@@ -39,6 +39,11 @@ public class CarTest {
 		assertThat(car1.getSpeed(), is(1));
 	}
 	// Create and pass test for no deceleration below 0.
-
+	@Test
+	public void cannotDecelerateBeyondMinSpeed(){
+		Car car1 = new Car("Red", 100);
+		car1.accelerate(0);
+		assertThat(car1.getSpeed(), is(0));
+	}
 	
 }
